d9d88dd426d6d6fbd875dbd42b992bf993119b8e
root <umair.k@careem.com>
2017-05-15 00:12:53 +0500

    Version 2

--- command.sh
+++ command.sh
@@ -1,3 +1,7 @@
 #jar -xvf sample.war
 jar -cvf sample.war hello.jsp images/ index.html  META-INF/ WEB-INF/
 rm -rf images/ index.html  META-INF/ WEB-INF/ hello.jsp
+git add .
+git commit -m "Version 2"
+git push -u origin master
+
