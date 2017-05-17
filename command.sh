32cb20f88655ef8edd77eaf399729a7e9223dab5
root <umair.k@careem.com>
2017-05-17 17:32:31 +0500

    Version 5

--- command.sh
+++ command.sh
@@ -2,6 +2,6 @@
 jar -cvf sample.war hello.jsp images/ index.html  META-INF/ WEB-INF/
 rm -rf images/ index.html  META-INF/ WEB-INF/ hello.jsp
 git add .
-git commit -m "Version 4"
+git commit -m "Version 5"
 git push -u origin master
-
+curl -H "Accept: application/json" -d roles="appserver" -d secretkey="replacethiswithsomethingbetter" -k http://34.250.152.1:8080/hook/deploy
